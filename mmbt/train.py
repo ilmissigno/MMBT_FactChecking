0c5ac1c747794594d96868ebf933634637919e1e
missigno <raff.formisano@studenti.unina.it>
2021-08-17 17:32:25 +0200

    doc_extraction_pt5

--- mmbt/train.py
+++ mmbt/train.py
@@ -477,29 +477,40 @@ def doc_feat_extraction(args, settings_dict):
     for i_epoch in range(start_epoch,args.max_epochs):
         logger.warning("*"*50+" EPOCH "+str(i_epoch)+" "+"*"*50)
         train_losses = []
-        similarities_res = []
+        total_similarities = []
         counterz = 0
         model.train()
         optimizer1.zero_grad()
         model.zero_grad()
-        for batch_d,batch_q in tqdm(zip(train_doc_loader,itertools.cycle(train_query_loader)), total=len(train_doc_loader)):
-            with torch.cuda.amp.autocast():
-                out_d,tgt = model_forward_feat(i_epoch, model, args, batch_d)
-                out_q,_ = model_forward_feat(i_epoch, model, args, batch_q)
-            res,loss = loss_obj(out_d,out_q,tgt)
-            if args.gradient_accumulation_steps > 1:
-                loss = loss / args.gradient_accumulation_steps
-            train_losses.append(loss.item())
-            loss.backward()
-            optimizer1.step()
-            optimizer1.zero_grad()
-            similarities_res.append(res.cpu().detach().numpy())
-            torch.cuda.empty_cache()
-            gc.collect()
+        iter_doc = iter(train_doc_loader)
+        for batch_d in tqdm(iter_doc, total=len(train_doc_loader)):
+            similarities_res = []
+            q_counterz = 0
+            iter_query = iter(train_query_loader)
+            for batch_q in tqdm(iter_query, total=len(train_query_loader)):
+                with torch.cuda.amp.autocast():
+                    out_d,tgt = model_forward_feat(i_epoch, model, args, batch_d)
+                    out_q,_ = model_forward_feat(i_epoch, model, args, batch_q)
+                res,loss = loss_obj(out_d,out_q,tgt)
+                if args.gradient_accumulation_steps > 1:
+                    loss = loss / args.gradient_accumulation_steps
+                train_losses.append(loss.item())
+                loss.backward()
+                optimizer1.step()
+                optimizer1.zero_grad()
+                similarities_res.append(res.cpu().detach().numpy())
+                torch.cuda.empty_cache()
+                gc.collect()
+                q_counterz+=1
+                if q_counterz == 100:
+                    break
+            #? Similarità qui tra singolo doc e query valutate (media?)
+            total_similarities.append(np.nanmean(similarities_res))
             counterz+=1
-            if counterz == 100:
+            if counterz == 2:
                 break
-        logger.info("Total similarity : "+str(np.nanmean(similarities_res)))
+            
+        logger.info("Total similarity : "+str(np.nanmax(total_similarities)))
         logger.info("Train Loss : "+str(np.nanmean(train_losses)))
     
 
